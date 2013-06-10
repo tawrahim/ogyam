f811675d4f0efcbc24ef7ed690703add060323db
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-06-10 19:46:14 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ogyam::Application.config.secret_token = '3a2ac6c92749e236dbec6490104267777e65c54d591daf88eb4ea1ccf09dd1bb87cbedb0489840576097c1ce5da16b1ca11c67ae82e086a285814f51733d5388'
